64a8e47d9fdbb5f3fbc20fe9dca8bce60a6662cb
Matthew Kling <mattkling@gmail.com>
2019-03-07 11:13:49 -0800

    update documentation and description

--- R/hydro.R
+++ R/hydro.R
@@ -1,9 +1,9 @@
 
 #' Extraterrestrial solar radiation, per Shuttleworth 1993
 #'
-#' @param psi Latitude, in degrees
+#' @param psi latitude, in degrees
 #' @param J Julian day
-#' @return Solar radiation in mm/day
+#' @return solar radiation in mm/day
 ETSR <- function(psi, J){
 
       # Shuttleworth's test:
@@ -66,7 +66,7 @@ monthly_S0 <- function(month, latitude){
 #' @param S0 numeric
 #' @param tmean degrees C
 #' @param tmin degrees C
-#' @param tmean degrees C
+#' @param tmax degrees C
 #' @return evapotranspiration (ETP) in mm/day
 hargreaves <- function(S0, tmean, tmin, tmax){
       0.0023 * S0 * (tmax - tmin)^.5 * (tmean + 17.8)
@@ -79,8 +79,8 @@ hargreaves <- function(S0, tmean, tmin, tmax){
 #' @param latitude degrees
 #' @param ppt vector of length 12, mm
 #' @param tmean vector of length 12, degrees C
+#' @param tmax vector of length 12, degrees C
 #' @param tmin vector of length 12, degrees C
-#' @param tmean vector of length 12, degrees C
 #' @return A named vector of hydrologic variables: PPT, PET, AET, CWD, RAR
 water_balance <- function(latitude, ppt, tmean, tmax, tmin){
 
@@ -113,6 +113,9 @@ water_balance <- function(latitude, ppt, tmean, tmax, tmin){
 #' @param already_latlong leave as F unless rasters are already in lat-long projection
 #' @return A raster layer with values representing degrees latitude
 latitude <- function(template, already_latlong=FALSE){
+
+      requireNamespace("raster")
+      requireNamespace("sp")
 
       lat <- template * 1 # force into RAM
       coords <- coordinates(lat)
@@ -147,14 +150,15 @@ latitude <- function(template, already_latlong=FALSE){
 #' @param ncores number of computing cores to use for parallel processing
 #' @param already_latlong leave as F unless rasters are already in lat-long
 #'   projection
+#' @param ... additional arguments to clusterR or writeRaster
 #' @return A raster stack with 5 layers: PPT, PET, AET, CWD, RAR
 #' @references Wang et al 2012 -- ClimateWNA -- Journal of Applied Meteorology
 #'   and Climatology. Shuttleworth 1993 -- Evaporation (chapter 4 in Handbook of
 #'   Hydrology). Hargreaves and Samani 1985 -- Reference Crop Evaporation from
 #'   Ambient Air Temperature
-hydro <- function(rasters, temp_scalar=1, ppt_scalar=1, ncores=1, already_latlong=F){
+hydro <- function(rasters, temp_scalar=1, ppt_scalar=1, ncores=1, already_latlong=F, ...){
 
-      require(raster)
+      requireNamespace("raster")
 
       # latitude raster
       lat <- latitude(rasters[[1]], already_latlong=already_latlong)
@@ -174,7 +178,8 @@ hydro <- function(rasters, temp_scalar=1, ppt_scalar=1, ncores=1, already_latlon
 
       beginCluster(ncores, type="SOCK")
       wb <- clusterR(rasters, calc, args=list(fun=w),
-                     export=c("ETSR", "hargreaves", "water_balance", "monthly_S0"))
+                     export=c("ETSR", "hargreaves", "water_balance", "monthly_S0"),
+                     ...)
       endCluster()
 
       names(wb) <- c("PPT", "PET", "AET", "CWD", "RAR")
